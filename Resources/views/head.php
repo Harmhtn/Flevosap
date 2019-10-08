fea9e460fb3126587c7cce3140d8d349cd6fcdd7
Maud <s1146976@student.windesheim.nl>
2019-10-08 14:02:52 +0200

    git pull

--- Resources/views/head.php
+++ Resources/views/head.php
@@ -3,7 +3,7 @@
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="Resources/css/style.css">
+    <link rel="stylesheet" href="src/Resources/public/css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
           integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <title>Flevosap</title>
@@ -12,5 +12,4 @@
 <body>
 
 <?php require "Resources/views/nav.php" ?>
-
 
