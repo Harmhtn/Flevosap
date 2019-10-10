474946f14fabfd868ce778b84e7d21869076bac5
Maud <s1146976@student.windesheim.nl>
2019-10-10 13:58:17 +0200

    speciaal voor sander

--- src/Controllers/admin.php
+++ src/Controllers/admin.php
@@ -18,6 +18,7 @@ if(!empty($_GET['action'])) {
         case "add":
             if (isset($_POST['add']))
             {
+
                 $file = $_FILES['image'];
 
                 $fileName = $_FILES['image']['name'];
@@ -37,7 +38,7 @@ if(!empty($_GET['action'])) {
                     {
                         if($fileSize > 20000){
                             $fileNameNew = uniqid('', true) . '.' . $fileExt;
-                            $fileDestination = 'scr/Resources/public/images/imageupload/' . $fileNameNew;
+                            $fileDestination = 'src/Resources/public/images/imageupload/' . $fileNameNew;
                             move_uploaded_file($fileTmpName,$fileDestination);
                             $_SESSION['picture'] = $fileNameNew;
                             $productById = $app['database']->createProduct();
@@ -73,7 +74,5 @@ require 'Resources/views/default/admin.view.php';
 
 //load footer
 require 'Resources/views/footer.php';
-echo "<pre>";
-   print_r($_POST['image']);
-   exit;
+
 
