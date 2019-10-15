2b1552aa51097d895be4ccb2336554266cc541b7
Harmhtn <Harmhtn@gmail.com>
2019-10-15 16:03:08 +0200

    navbar gefixed

--- Resources/views/nav.php
+++ Resources/views/nav.php
@@ -1,14 +1,16 @@
-<div class="d-flex flex-md-row align-items-center bg-white">
-    <a class="my-0 mr-md-auto" href=""><img style="max-height: 120px;" class="" src="src/Resources/public/images/logo-nav.png" alt=""></a>
-    <nav class="navbar navbar-light bg-light">
+<link rel="stylesheet" href="src/Resources/public/css/navbarstyle.css">
+
+<div class="container d-flex flex-md-row align-items-center bg-white">
+    <nav class="navbar navbar-light bg-light bg-white">
+        <a class="my-0 mr-md-auto" href=""><img style="max-height: 120px;" class="" src="src/Resources/public/images/logo-nav.png" alt=""></a>
         <a class="p-2 text-dark" href="#">Boomgaard</a>
         <a class="p-2 text-dark" href="#">In de fles</a>
         <a class="p-2 text-dark" href="#">Hmmm</a>
         <a class="p-2 text-dark" href="#">Sappen</a>
         <a class="p-2 text-dark" href="#">Horeca</a>
         <a class="p-2 text-dark" href="#">Nieuws</a>
-        <a  class="p-2 text-dark" href="#">Contact</a>
-        <a  class="p-2 text-dark" href="/logout">Logout</a>
+        <a class="p-2 text-dark" href="#">Contact</a>
+        <a class="p-2 text-dark" href="logout">Logout</a>
     </nav>
     <a class="fas fa-shopping-cart"></a>
 </div>
