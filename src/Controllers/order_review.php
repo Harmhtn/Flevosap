46027827b95cb44f4490f7c8f354a2c38b59b443
Harmhtn <Harmhtn@gmail.com>
2019-10-18 21:55:04 +0200

    fix

--- src/Controllers/order_review.php
+++ src/Controllers/order_review.php
@@ -6,11 +6,11 @@ $table = 'customers';
 $userdId = $_SESSION['user_id'];
 $user_data = $app['database']->selectUserAddress($table, $userdId);
 
-//if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-//    //alle $_POST values
-//    $newAddress = $_POST['newAddress'];
-//    $paymentMethod = $_POST[''];
-//}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    //alle $_POST values
+    $newAddress = $_POST['newAddress'];
+    $paymentMethod = $_POST['select_payment'];
+}
 
 require 'Resources/views/default/order_review.view.php';
 
