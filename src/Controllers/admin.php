920f87ff256978140605bcc4fc7f54d839e5e81f
Maud <s1146976@student.windesheim.nl>
2019-10-18 16:09:39 +0200

    commiting to pull

--- src/Controllers/admin.php
+++ src/Controllers/admin.php
@@ -2,23 +2,18 @@
 <?php
 session_start();
 
-
-
-if(!empty($_GET['action'])) {
+if (!empty($_GET['action'])) {
     switch ($_GET['action']) {
-        case "remove":
+        case 'remove':
 
-            if (isset($_GET['id']))
-            {
+            if (isset($_GET['id'])) {
                 $id = $_GET['id'];
                 $productById = $app['database']->removeItem($id);
             }
 
             break;
-        case "add":
-            if (isset($_POST['add']))
-            {
-
+        case 'add':
+            if (isset($_POST['add'])) {
                 $file = $_FILES['image'];
 
                 $fileName = $_FILES['image']['name'];
@@ -30,33 +25,24 @@ if(!empty($_GET['action'])) {
                 $fileExtTmp = explode('.', $fileName);
                 $fileExt = strtolower(end($fileExtTmp));
 
-                $allowed = array('jpg', 'jpeg', 'png');
+                $allowed = ['jpg', 'jpeg', 'png'];
                 $_SESSION[''] = '';
-                if(in_array($fileExt, $allowed))
-                {
-                    if($fileError === 0)
-                    {
-                        if($fileSize > 20000){
+                if (in_array($fileExt, $allowed)) {
+                    if ($fileError === 0) {
+                        if ($fileSize > 20000) {
                             $fileNameNew = uniqid('', true) . '.' . $fileExt;
                             $fileDestination = 'src/Resources/public/images/imageupload/' . $fileNameNew;
-                            move_uploaded_file($fileTmpName,$fileDestination);
+                            move_uploaded_file($fileTmpName, $fileDestination);
                             $_SESSION['picture'] = $fileNameNew;
                             $productById = $app['database']->createProduct();
+                        } else {
+                            echo 'The picture is to big';
                         }
-                        else
-                        {
-                            echo "The picture is to big";
-
-                        }
+                    } else {
+                        echo 'Er is een fout opgetreden.' . $fileError;
                     }
-                    else
-                    {
-                        echo "Er is een fout opgetreden.". $fileError;
-                    }
-                }
-                else
-                {
-                    echo "Dit type bestand is niet toegestaan.";
+                } else {
+                    echo 'Dit type bestand is niet toegestaan.';
                 }
             }
 
@@ -74,5 +60,3 @@ require 'Resources/views/default/admin.view.php';
 
 //load footer
 require 'Resources/views/footer.php';
-
-
