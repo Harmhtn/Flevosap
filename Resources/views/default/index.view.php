9870a95a59d81218511158a814c3b57ff7b0bb6e
Sanderboots1 <sanderbootsman@gmail.com>
2019-10-22 09:58:49 +0200

    product is now added on index

--- Resources/views/default/index.view.php
+++ Resources/views/default/index.view.php
@@ -33,8 +33,8 @@ foreach ($products as $product) {
     </div>
     <div class='col-md-6'>
         <div class='card-body'>
-            <form action='winkelmand?action=add&id=". $product['product_id'] ."' method='post'>
-            <button type=\"submit\" class=\"btn btn-info float-right mb-2\">
+            <form action='?action=add&id=". $product['product_id'] ."'  method='post'>
+            <button type=\"submit\" onclick='added()' class=\"btn btn-info float-right mb-2\">
             <i class=\"fas fa-cart-arrow-down\"></i>
             </button>
             <div class=\"form-group\">
@@ -52,11 +52,24 @@ foreach ($products as $product) {
     <div class='col-md-2'></div>
 
 </div>
-<hr>
+
+<div class=\"modal fade\" id=\"myModal\" role=\"dialog\">
+    <div class=\"modal-dialog\">
+      
+    </div>
+  </div>
 ";
 }
 
 ?>
+<script>
+    //make alert after reload.
+    function added() {
+        window.alert("Het product is toegevoegd aan de winkelmand");
+    }
+</script>
+
+
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
